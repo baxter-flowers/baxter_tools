c6bc26621e2f44aae4674274aa8c52cefac0f8d8
Yoan Mollard <yoan.mollard@inria.fr>
2015-06-23 12:38:21 +0200

    Fixed the left + right camera recorder

--- scripts/camera_recorder.py
+++ scripts/camera_recorder.py
@@ -6,10 +6,10 @@ import rospy, cv_bridge
 from copy import deepcopy
 from baxter_interface.camera import CameraController
 from sensor_msgs.msg import Image
+from threading import Lock
 
 class BaxterVideoRecorder():
     def open_camera(self, camera, x_res, y_res):
-        print x_res, y_res, "haha"
         if camera == "left":
             cam = CameraController("left_hand_camera")
         elif camera == "right":
@@ -18,10 +18,9 @@ class BaxterVideoRecorder():
             cam = CameraController("head_camera")
         else:
             sys.exit("ERROR - open_camera - Invalid camera")
-        cam.close()
+
         # set camera parameters
         cam.resolution          = (int(x_res), int(y_res))
-        print cam.resolution
         cam.exposure            = -1             # range, 0-100 auto = -1
         cam.gain                = -1             # range, 0-79 auto = -1
         cam.white_balance_blue  = -1             # range 0-4095, auto = -1
@@ -45,7 +44,8 @@ class BaxterVideoRecorder():
         cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGRA2BGR)  # Remove depth
         if self.recording:
             self.left_writer.write(cv_image)
-        cv2.imshow("Left image", cv_image)
+        with self.lock_cv:
+            cv2.imshow("Left image", cv_image)
         cv2.waitKey(1)
 
     def right_handler(self, image):
@@ -54,22 +54,27 @@ class BaxterVideoRecorder():
         cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGRA2BGR)  # Remove depth
         if self.recording:
             self.right_writer.write(cv_image)
-        cv2.imshow("Right image", cv_image)
+        with self.lock_cv:
+            cv2.imshow("Right image", cv_image)
         cv2.waitKey(1)
 
     def __init__(self, width, height):
         self.recording = False
+        self.lock_cv = Lock() # openCV has issues with concurrent imshow
 
-        # Tricky behaviour: 2 cameras are always kept opened with L/R opened by default
-        # Closing a closed camera raises an unclear error, so let's work with default openings
+        # Tricky behaviour: 2 cameras are always kept opened with L/R powered but not opened by default
+        # Closing a closed camera raises an unclear error, so let's catch a possible failure during closure
         # https://groups.google.com/a/rethinkrobotics.com/d/msg/brr-users/VPqJDkXTVzE/4uk93xRmtQwJ
 
-        #print "Closing head cam..."
-        #self.close_camera("head")
-        #print "Opening left cam..."
-        #self.open_camera("left", width, height)
-        #print "Opening right cam..."
-        #self.open_camera("right", width, height)
+        print "Closing head cam..."
+        try:
+            self.close_camera("head")
+        except AttributeError:
+            print "Probably already closed"
+        print "Opening left cam..."
+        self.open_camera("left", width, height)
+        print "Opening right cam..."
+        self.open_camera("right", width, height)
 
         self.left_writer = None
         self.right_writer = None
@@ -93,8 +98,5 @@ class BaxterVideoRecorder():
         self.right_writer.release()
 
 if __name__ == '__main__':
-    rospy.init_node('recorder')
+    rospy.init_node('lr_camera_recorder')
     BaxterVideoRecorder(1280, 800)
-
-
-
